7b57e2d8f10289f2496ba5d7b4e991421af51533
Abigail Zeke <62107806+abigailzeke@users.noreply.github.com>
2020-03-14 04:03:53 +0800

    remove api

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post("/",function(req,res){
   const jsonData = JSON.stringify(data);
   const options = {
     method: "POST",
-    auth: "abigailpro:7a7e60b4254d6f4f0e69627a4962b522-us3"
+    auth: "yourMailChimpUsername:yourMailChimpAPI"
   }
 
   const request = https.request("https://us3.api.mailchimp.com/3.0/lists/ca4b741770",options,function(response){
@@ -59,7 +59,3 @@ app.post("/",function(req,res){
 app.post("/failure",function(req,res) {
   res.redirect("/");
 });
-
-// api key 7a7e60b4254d6f4f0e69627a4962b522-us3
-// unique id ca4b741770
-// {"name":"Freddie'\''s Favorite Hats","contact":{"company":"Mailchimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}
